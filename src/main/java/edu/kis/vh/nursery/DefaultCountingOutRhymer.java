88f174db673b70093cbe7dda0eac7efbbbb6cb1d
Jakub Chrabąszczewski <other.email@wp.pl>
2022-01-21 13:32:42 +0100

    1.5.6 Generowanie schematu dokumentacji i uzupełnienie

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,10 @@
 package edu.kis.vh.nursery;
 
+/**
+ * @author Jakub
+ * Klasa służy do dostarczania różnych wariantów wylicznek, w swoim działaniu przypomina stos.
+ *
+ */
 public class DefaultCountingOutRhymer {
 
 	public static final int STARTING_VALUE = -1;
@@ -9,25 +14,46 @@ public class DefaultCountingOutRhymer {
 
 	public int total = STARTING_VALUE;
 
+	/**
+	 * @param in
+	 * Metoda sprawdza czy tablica numbers jest pełna, jeśli nie to wpisuje wartość in do tablicy. 
+	 * 
+	 */
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
+		/**
+		 * @return
+		 * Metoda zwraca True jeśli wartość total jest taka sama jak STARTING_VALUE
+		 */
 		public boolean callCheck() {
 			return total == STARTING_VALUE;
 		}
 		
+			/**
+			 * @return
+			 * Metoda zwraca True jeśli wartość total jest taka sama jak FULL
+			 */
 			public boolean isFull() {
 				return total == FULL;
 			}
 		
+				/**
+				 * @return
+				 * Metoda zwraca elementy tablicy 
+				 */
 				protected int peekaboo() {
 					if (callCheck())
 						return STARTING_VALUE;
 					return numbers[total];
 				}
 			
+					/**
+					 * @return
+					 * Metoda zwraca poprzedni element tablicy 
+					 */
 					public int countOut() {
 						if (callCheck())
 							return STARTING_VALUE;
